ccbd6b730282205d56086b615766b1849142ccc0
GriffGreen <saveoursonics@gmail.com>
2016-01-28 12:53:55 -0800

    Readjusted notes due to Ethereum Wallet bug
    The Ethereum wallet cant handle the @ signs even when commented out

--- WillManager.sol
+++ WillManager.sol
@@ -1,10 +1,10 @@
-/// @title: Will Manager to for a Non-Devs' Enjoyment!
-/// @author: Griff Green
-/// @notice: This Contract gives the user a chance to interact with the blockchain as a novice, 
+/// title: Will Manager to for a Non-Devs' Enjoyment!
+/// author: Griff Green
+/// notice: This Contract gives the user a chance to interact with the blockchain as a novice, 
 ///it is not really for Will Validation
 
 contract WillManager {
-	/// @notice: Making these variables public displays them in the Ethereum Wallet.
+	/// notice: Making these variables public displays them in the Ethereum Wallet.
 	address public willOwner;
 	bytes32 public hashOfWill;
 	bytes32 public hashOfWillBeingChecked;
@@ -12,21 +12,20 @@ contract WillManager {
 	bool public willCreated;
 	address public lastWillChecker;
 	
-	/// @notice: This function is called when the contract is deployed and it sets the account 
+	/// notice: This function is called when the contract is deployed and it sets the account 
 	///deploying it as the 'Will owner'	
 	function WillManager(){
 	    	willOwner = msg.sender;
 	}
 	
-	/// @notice: In case you sent ether to this by accident this function will send the funds 
+	/// notice: In case you sent ether to 'this' by accident this function will send the funds 
 	///to the 'Will owner.' Don't send funds to this contract
 	function empty(){
  		uint256 balance = address(this).balance;
      		address(willOwner).send(balance);
 	}
-	/// @notice: This function will only update the 'Hash of will' if called by the 'Will owner'
-	///otherwise it will make 'will Created' out put 'No' 
-	/// @param: string will The current version of the will that the user wants in the blockchain
+	/// notice: This function will only update the 'Hash of will' if called by the 'Will owner'
+	///otherwise it will make 'Will created' display 'No' 
 	function newWill(string will) {
     		if (msg.sender != willOwner) {
         		willCreated = false;
@@ -35,9 +34,8 @@ contract WillManager {
             	willCreated = true;
     	}
 	}
-	/// @notice: This function can be called by anyone and is used to verify that the version
+	/// notice: This function can be called by anyone and is used to verify that the version
 	///of the Will that they have is the current up-to-date version
-	/// @param: string willUserIsChecking The version of the will that the user wants to verify
 	function checkWill(string willUserIsChecking) {
                 lastWillChecker = msg.sender;
                 hashOfWillBeingChecked = sha3(willUserIsChecking);  
